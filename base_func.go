c331b6ec2dcfd04c3e9ac57e0f56fbcf07e0130a
freemanmengyuan <1647786154@qq.com>
2019-03-19 22:14:48 +0800

    control flow over

--- base_func.go
+++ base_func.go
@@ -16,14 +16,25 @@ func calculateBillSimple(num, price int) int {
 }
 
 //多返回值
+func rectProps(length, width float64) (float64, float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area, perimeter
+}
 
 //命名返回值
-
-//空白符
+func rectPropsName(length, width float64) (area float64, perimeter float64) {
+	area = length * width
+	perimeter = (length + width) * 2
+	return
+}
 
 func main() {
 
 	price, num := 4, 5                     //定义变量
 	ret := calculateBillSimple(num, price) //调用
 	fmt.Println(ret)
+	//空白符
+	var a, _ = rectPropsName(2, 4)
+	fmt.Printf("the area is %.2f \n", a)
 }
