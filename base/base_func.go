c7150c662881e38fd98a08f6ed98da6c4e73629a
mengyuan <mengyuan.bj@acewill.cn>
2019-07-20 23:34:31 +0800

    restart

---
+++ base/base_func.go
@@ -0,0 +1,40 @@
+package base
+
+import "fmt"
+
+// 声明
+func calculateBill(num int, price int) int {
+	ret := num * price
+	return ret
+}
+
+// 参数 和 返回值类型都是可选的
+// 同类型的参数可以简写
+func calculateBillSimple(num, price int) int {
+	ret := num * price
+	return ret
+}
+
+//多返回值
+func rectProps(length, width float64) (float64, float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area, perimeter
+}
+
+//命名返回值
+func rectPropsName(length, width float64) (area float64, perimeter float64) {
+	area = length * width
+	perimeter = (length + width) * 2
+	return
+}
+
+func main() {
+
+	price, num := 4, 5                     //定义变量
+	ret := calculateBillSimple(num, price) //调用
+	fmt.Println(ret)
+	//空白符
+	var a, _ = rectPropsName(2, 4)
+	fmt.Printf("the area is %.2f \n", a)
+}
---
+++ base/base_point.go
@@ -0,0 +1,27 @@
+package base
+
+import "fmt"
+
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+
+func swapNew(a, b int) (int, int) {
+	a, b = b, a
+	return a, b
+}
+func main() {
+
+	// 指针的典型例子 间接的更改变量的值 是指针存在的根本意义
+	var a int = 2
+	var pa *int = &a
+	*pa = 3
+	fmt.Println(a)
+
+	// go只有值传递 没有引用传递
+	// 交换变量的值
+	a, b := 3, 4
+	//swap(&a, &b)
+	a, b = swapNew(a, b)
+	fmt.Println(a, b)
+}
