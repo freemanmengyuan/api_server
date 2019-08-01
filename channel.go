09adbb42dedab481def32aec8632199c93c5068f
mengyuan <mengyuan.bj@acewill.cn>
2019-08-02 00:33:19 +0800

    channel

--- channel.go
+++ channel.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+//利用通信来共享内存 而不是利用共享内存来通信
 /*func channelDemo() {
 
 	//var c chan int //c=nil
@@ -65,6 +66,38 @@ func main() {
 	channelDemo()
 }*/
 
+//chan 作为返回值
+/*func work(id int, c chan int) {
+	for a := range c {
+		fmt.Printf("worker %d received %c \n", id, a)
+		//done <- true
+		//done chan bool
+	}
+
+}
+
+//返回channel
+func createWork(id int) chan<- int {
+	c := make(chan int)
+	go work(id, c)
+	return c
+}
+
+func channelDemo() {
+	var channels [10]chan<- int
+	//done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		channels[i] = createWork(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 'a' + i
+	}
+
+	time.Sleep(time.Millisecond)
+	//fmt.Print(a)
+}*/
+
 // channelbuffer channel的缓冲区
 //如果没有接收方 加入buffer也不会	dead lock
 /*func worker(id int, c chan int) {
@@ -102,6 +135,7 @@ func worker(id int, c chan int) {
 		fmt.Printf("worker %d received %c", id, a)
 		fmt.Println()
 	}*/
+	//改进 解决打印空串
 	for {
 		a, ok := <-c
 		if ok {
